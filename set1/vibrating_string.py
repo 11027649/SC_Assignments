4e0c8db7869a35266dcb3ee6c14ca3e44ba192d6
11027649 <natasjawezel@hotmail.com>
2019-02-12 14:32:14 +0100

    plot with waves at different timesteps

--- set1/vibrating_string.py
+++ set1/vibrating_string.py
@@ -10,26 +10,40 @@ def main():
     # variables
     c = 1.0
     dt = 0.001
+    tmax = 1.5
+    timesteps = math.ceil(tmax/dt)
 
     # global variables needed for animation
     global current_state, fig, ax1
 
     # initalize wave, pick 2pi, 5pi or 5pi_constrained_domain
-    current_state = Wave("2pi", c, dt)
+    current_state = Wave("5pi_constrained_domain", c, dt)
 
-    # initalize figure
-    fig = plt.figure()
-    fig.set_dpi(100)
-    fig.suptitle("Vibrating String")
+    # ask user what to do
+    print("Do you want to see the animation of the wave? Yes/No", end=" ")
+    visualization = input()
 
-    ax1 = fig.add_subplot(1,1,1)
+    if visualization == "no":
+        for t in range(timesteps):
+            current_state.next_step()
 
-    # animate
-    anim = animation.FuncAnimation(fig, animate, frames=500, interval=50)
-    anim.save('vibrating_string_'+ current_state.type + '.mp4')
+        current_state.plot_time_frames()
+    elif visualization == "yes":
+        #### ANIMATION
+        # initalize figure
+        fig = plt.figure()
+        fig.set_dpi(100)
+        fig.suptitle("Vibrating String")
 
-    plt.show()
+        ax1 = fig.add_subplot(1,1,1)
 
+        # animate
+        anim = animation.FuncAnimation(fig, animate, frames=timesteps, interval=100)
+        plt.show()
+        print("animation done")
+        # save animation
+        anim.save('vibrating_string_'+ current_state.type + '.mp4')
+        print("animation is saved")
 
 def animate(i):
     """ Calculate next state and set that for the animation. """
@@ -47,11 +61,15 @@ class Wave():
         self.type = type
         self.x = np.linspace(0, 1, 100)
         self.dx = 1/len(self.x)
+        self.dt = dt
 
         self.constant = (c * dt/self.dx)**2
 
         self.y_previous = []
         self.y_current = []
+
+        self.timeframes = {}
+        self.timestep = 0
 
         self.initialize()
 
@@ -94,8 +112,33 @@ class Wave():
 
         y_next.append(0)
 
-        self.y_previous = copy.deepcopy(self.y_current)
-        self.y_current = copy.deepcopy(y_next)
+        self.y_previous = copy.copy(self.y_current)
+        self.y_current = copy.copy(y_next)
+
+        if self.timestep % 100 is 0:
+            self.timeframes[self.timestep] = copy.copy(self.y_current)
+
+        self.timestep += 1
+
+
+    def plot_time_frames(self):
+        """ Make a plot with the wave at different time points. """
+
+        fig = plt.figure()
+        plt.grid(True)
+
+        plt.ylim([-1.5,1.5])
+        plt.xlim([0,1])
+
+        for key in self.timeframes.keys():
+            if key == 0:
+                plt.plot(self.x, self.timeframes[key], label="time: " + str(round(key*self.dt, 3)), linewidth=5)
+            else:
+                plt.plot(self.x, self.timeframes[key], label="time: " + str(round(key*self.dt, 3)))
+
+        plt.title("Wave at different times")
+        plt.legend(loc="upper right")
+        plt.show()
 
 
 if __name__ == '__main__':
