d9ba3f8dfc65fa149876e097c489ab7b2f40a75c
RomyM08 <rm.meester@gmail.com>
2019-02-28 21:59:17 +0100

    Update RandomWalker.py
    There is still a bug... Als de walker over het object gaat, verwijdert hij het object.

--- set2/RandomWalker.py
+++ set2/RandomWalker.py
@@ -20,19 +20,14 @@ import time
 import random
 
 class RandomWalker():
-    """ This is a class that contains the RandomWalker aka Monte Carlo simulation."""
+    """ This is a class that contains the random walker aka
+        the Monte Carlo simulation."""
 
     def __init__(self, gridsize):
         self.height = gridsize
         self.width = gridsize
 
         self.step = 0
-
-        self.saved_states = {}
-
-        self.reached_boundaries = False
-        self.candidates = []
-
 
         self.initialize()
 
@@ -48,38 +43,65 @@ class RandomWalker():
 
     def place_walker(self):
         """ This is a function that places a new random walker at the top of the
-            grid.
-            TODO: check if there's not already an object there. """
+            grid. """
 
         print(self.step, "placing a walker")
 
         self.walker_x = random.choice(range(self.width))
-        self.walker_y = 0
+        # self.walker_y = 0
+        self.walker_y = self.height -2
+
+        # check if there's an object at this grid point
+        if self.grid[self.walker_y][self.walker_x] == 1:
+            self.place_walker()
 
         # initialize random walker seed with concentration 2 on top of grid
         self.grid[self.walker_y][self.walker_x] = 2
+
 
     def remove_walker(self):
         """ This is a function that removes the random walker. """
 
         self.grid[self.walker_y][self.walker_x] = 0
 
+    def define_neighbours(self):
+        """ This function defines the neighbours of the random walker. """
+        self.neighbours = []
+
+        # check out of bound (down, up, left, right), and when not, append neighbour
+        #MAAR DE HOEKPUNTEN KLOPPEN ZO VOLGENS MIJ HIER OOK WEER NIET...
+        if not self.walker_y < 0:  #down
+            self.neighbours.append(self.grid[self.walker_y - 1][self.walker_x])
+        elif not self.walker_y == self.height: #up
+            self.neighbours.append(self.grid[self.walker_y + 1][self.walker_x])
+        elif not self.walker_x < 0: #left
+            self.neighbours.append(self.grid[self.walker_y][self.walker_x - 1])
+        elif not self.walker_x == self.width: #right
+            self.neighbours.append(self.grid[self.walker_y][self.walker_x + 1])
 
     def next_step(self):
+        """ The next step of the animation. """
+        #To Do: append statement to only create a next step when the
+        # random walker has been added to the object. --
+        # HAS BEEN ADDED IN COMMENTS
+
         # calculate new coordinates
         self.next_coordinates()
         # move there (or not)
         self.move()
 
-        # check
-        # # check if it aggregates
-        # if #check whether its a neighbour
-        #     # add to objects
-        #     self.object_grid[x][y] = 1
-        #
-        #     # remove from candidates
-        #     self.candidates.remove(coord)
-        # elif #check out of bound: same as self.check_boundaries():??
+        # check if walker aggregates by checking the neighbours
+        self.define_neighbours()
+        for neighbour in self.neighbours:
+            if neighbour == 1:
+                # walker becomes part of the object
+                self.grid[self.walker_y][self.walker_x] == 1
+
+                # place a new random walker
+                self.place_walker()
+
+                # next step of the animation when random walker is aggregated
+                # self.step += 1
 
         self.step += 1
 
@@ -88,7 +110,7 @@ class RandomWalker():
 
         print(self.next_walker_x, self.next_walker_y)
 
-        # out of bound top/bottom
+        # out of bound at top/bottom
         if self.next_walker_y < 0 or self.next_walker_y == self.height:
             print("removing: " , self.grid[self.walker_y][self.walker_x])
             # remove this walker
@@ -97,7 +119,7 @@ class RandomWalker():
             self.place_walker()
         # else move walker
         else:
-            # out of bound - periodic boundary
+            # out of bound for periodic boundary
             if self.next_walker_x < 0:
                 self.next_walker_x = self.width - 1
             elif self.next_walker_x  == self.width:
@@ -122,7 +144,7 @@ class RandomWalker():
         self.next_walker_x = self.walker_x
         self.next_walker_y = self.walker_y
 
-        # decide next step
+        # make next step
         if direction is "left":
             self.next_walker_x = self.walker_x - 1
         elif direction is "right":
