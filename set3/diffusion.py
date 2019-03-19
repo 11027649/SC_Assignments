daec4fe98eb9ec0e7a9fea04e24ab22024820444
11027649 <natasjawezel@hotmail.com>
2019-03-19 11:30:40 +0100

    setups for next assignments

---
+++ set3/diffusion.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy as sp
+from scipy import linalg
+from scipy.sparse import linalg as linalg2
+
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import math
+import seaborn as sns
+sns.set()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
